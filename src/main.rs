8c325a4070a6cd9afc76bc07f2b8aa4a4734003b
Paul Mayer <paul@paul-mayer.eu>
2021-10-11 23:12:33 +0200

    contact test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ extern crate nalgebra as na;
 
 use na::{Isometry2, Point2, Translation2, Vector2};
 use ncollide2d::bounding_volume::{self, BoundingVolume};
-use ncollide2d::shape::Ball;
+use ncollide2d::query;
+use ncollide2d::shape::{Ball, Compound, Cuboid, ShapeHandle};
 
 fn main() {
     let ball1 = Ball::new(0.5);
@@ -27,4 +28,33 @@ fn main() {
     println!("{:?}", ball1_pos);
 
     println!("{:?}", aabb_ball1.intersects(&aabb_ball2));
+
+    let cuboid = Cuboid::new(Vector2::new(1.0, 1.0));
+    let ball = Ball::new(1.0);
+    let prediction = 1.0;
+
+    let cuboid_pos = na::one();
+    let ball_pos_penetrating = Isometry2::new(Vector2::new(1.0, 1.0), na::zero());
+    //let ball_pos_in_prediction = Isometry2::new(Vector2::new(2.0, 2.0), na::zero());
+    let ball_pos_too_far = Isometry2::new(Vector2::new(3.0, 3.0), na::zero());
+
+    let ctct_penetrating = query::contact(
+        &ball_pos_penetrating,
+        &ball,
+        &cuboid_pos,
+        &cuboid,
+        prediction,
+    );
+
+    println!("{:?}", ctct_penetrating);
+
+    let too_far = query::contact(
+        &ball_pos_penetrating,
+        &ball,
+        &cuboid_pos,
+        &cuboid,
+        prediction,
+    );
+
+    println!("{:?}", too_far);
 }
